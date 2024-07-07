ee0f02eff3e3b7e1a6f698c63708995bd8e72909
afrentador <vmendozaespina@gmail.com>
2024-07-07 10:01:01 -0500

    retomando conosimientos

---
+++ ejerciciosDia_1/index.js
@@ -0,0 +1,49 @@
+//R1: Primer comentario
+//R2: Bienvenido a 30DaysOfJavaScript
+/*
+R3: Los comentarios pueden hacer que
+ el codigo sea legible, facil de 
+ reutilizar e informativo.
+*/
+//R4:
+let stream = "cadena"
+let booleanA = true
+let booleanB = false
+let indefinido
+let nulo = null
+
+//R5: datatype
+console.log(typeof stream)
+console.log(typeof booleanA)
+console.log(typeof booleanB)
+console.log(typeof indefinido)
+console.log(typeof nulo)
+
+//R6:
+let team
+let bolan
+let fino
+let culo
+
+//R7:
+let nombre = "Victor"
+let apellido = "Mendoza"
+let edad = 40
+let profecion = "developer"
+
+//R8:
+let nombre2 = "Luis"
+let apellido2 = "Espina"
+let estadoCivil = "Casado"
+let pais = "Venezuela"
+let edad2 = 40
+
+//R9:
+let name = "Abraham", lastName = "Mendoza", countri = "Venezuela", age = "15"
+
+//10
+let myAge = 40
+let yourAge = 37
+
+console.log("I am " + myAge + " years old");
+console.log("Your are " + yourAge + " years old");
