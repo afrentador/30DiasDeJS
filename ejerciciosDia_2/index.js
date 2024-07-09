b0611bffb4c151d31a4ac3e97c4cb97ff82f0f32
afrentador <vmendozaespina@gmail.com>
2024-07-09 11:13:38 -0500

    ejercicios del dia 2

---
+++ ejerciciosDia_2/index.js
@@ -0,0 +1,53 @@
+//R1:
+let desafio = '30 Dias de JavaScript'
+//R2:
+console.log(desafio);
+//R3:
+console.log(desafio.length);
+//R4:
+console.log(desafio.toUpperCase());
+//R5:
+console.log(desafio.toLowerCase());
+//R6:
+console.log(desafio.substring(3, 7));
+//R7:
+console.log(desafio.substring(3));
+//R8:
+console.log(desafio.includes('Script'));
+//R9:
+console.log(desafio.split(''));
+//R10:
+console.log(desafio.split(' '));
+//R11:
+let plataformas = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+console.log(plataformas.split(', '));
+//R12:
+console.log(desafio.replace('JavaScript', 'Python'));
+//R13:
+console.log(desafio.charAt(15));
+//R14:
+console.log(desafio.charCodeAt(11));
+//R15:
+console.log(desafio.indexOf('a'));
+//R16:
+console.log(desafio.lastIndexOf('a'));
+//R17:
+let oración = 'No puedes terminar una oración con porque porque porque es una conjunción'
+console.log(oración.indexOf('porque'));
+//R18:
+console.log(oración.lastIndexOf('porque'));
+//R19:
+console.log(oración.search('porque'));
+//R20:
+console.log(desafio.trim());
+//R21:
+console.log(desafio.startsWith('30 Dias'));
+//R22:
+console.log(desafio.endsWith('Script'));
+//R23:
+console.log(desafio.match(/a/g));
+//R24:
+let cadena = '30 Dias de '
+console.log(cadena.concat('JavaScript'));
+//R25:
+console.log(desafio.repeat(2));
