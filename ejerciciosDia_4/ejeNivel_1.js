a60a7d2eb4b58710b44ec190165f07156de558b0
afrentador <vmendozaespina@gmail.com>
2024-09-11 07:40:11 -0500

    retomanto tareas

--- ejerciciosDia_4/ejeNivel_1.js
+++ ejerciciosDia_4/ejeNivel_1.js
@@ -2,30 +2,77 @@
 console.log('Nivel 1');
 
 //R1:Obtenga la entrada del usuario usando el aviso ("Ingrese su edad:"). Si el usuario tiene 18 años o más, muestre el mensaje: 'Tiene la edad suficiente para conducir', pero si no tiene 18 años, brinde otro mensaje que indique que debe esperar la cantidad de años que necesita para cumplir 18.
-
+//RESUELTO=>
     //Ingrese su edad: 30
     //Tiene la edad suficiente para conducir.
-
+    
     //Ingrese su edad:15
     //Te faltan 3 años para conducir.
 
-//R2:Compara los valores de myAge y yourAge usando if... else. Según la comparación, registre el resultado en la consola indicando quién es mayor (tú o yo). Utilice prompt(“Ingrese su edad:”) para obtener la edad como entrada.
+let edadUsuario = 15
+//let edadUsuario = Number(prompt('Ingresa tu Edad'))
+let mayoriaEdad = 18 - edadUsuario
 
+ if (edadUsuario < 18){
+    console.log(`Aun te faltan cumplir ${mayoriaEdad} años para conducir`); 
+ } else {
+    console.log(`Ya tienes ${edadUsuario} edad suficiente para Conducir`);
+ }
+
+//R2:Compara los valores de myAge y yourAge usando if... else. Según la comparación, registre el resultado en la consola indicando quién es mayor (tú o yo). Utilice prompt(“Ingrese su edad:”) para obtener la edad como entrada.
+//RESUELTO=>
     //Ingrese su edad: 30
     //Eres 5 años mayor que yo.
+let tuEdad = 30
+//let tuEdad = Number(prompt(`Ingresa tu edad`))
+let miEdad = 25
+//let miEdad = Number(prompt(`Mi tu edad`))
+
+if (tuEdad > miEdad) {
+    console.log(`Eres ${tuEdad - miEdad} años mayor que yo`);
+} else {
+    console.log(`Yo soy mayor que tu`);
+}
 
 //R3:Si a es mayor que b, devuelve 'a es mayor que b'; de lo contrario, 'a es menor que b'. Trate de implementarlo de maneras diferentes
+//RESUELTO=>
+//4 es mayor que 3
+let a = 4;
+let b = 3;
 
-    //.Usando if else
-    //..Operador ternario.
-    let a = 4;
-    let b = 3;
-    //4 es mayor que 3
+//.Usando if else
+if(a > b){
+    console.log('a es mayor que b');
+}else{
+    console.log('b es mayor que a'); 
+}
 
+//..Operador ternario.
+ let mayor = a < b
+    mayor 
+    ? console.log('a es menor que b')
+    : console.log('b es menor que a');
+    
 //R4:Los números pares son divisibles por 2 y el resto es cero. ¿Cómo verificar si un número es par o no usando JavaScript?
-
+//RESUELTO=>
     // Ingrese un número: 2
     // 2 es un número par
-
+    
     // Ingrese un número: 9
     // 9 es un número impar
+    
+let valorAleatorio = 9
+//let valorAleatorio = Number(prompt(`Ingresa un Numero`))
+let resto = valorAleatorio % 2
+
+//.Usando if else
+if(valorAleatorio % 2){
+    console.log(valorAleatorio +' '+'es un número impar');
+} else {
+    console.log(valorAleatorio +' '+'es un número par');
+}
+
+//..Operador ternario.
+resto
+? console.log(valorAleatorio +' '+'es un número impar')
+: console.log(valorAleatorio +' '+'es un número par');
--- ejerciciosDia_4/ejeNivel_2.js
+++ ejerciciosDia_4/ejeNivel_2.js
@@ -2,22 +2,103 @@
 console.log('nivel 2')
 
 //R1:Escriba un código que pueda calificar a los estudiantes de acuerdo con sus puntajes:
-
-    // 80-100, A
+    // 90-100, A
     // 70-89, B
     // 60-69, C
     // 50-59, D
     // 0-49, F
+//RESUELTO=>
+// con IF ELSE
+let calificacion = 30
+//let calificacion = Number(prompt(`Ingresa la Calificacion`))
 
+if(calificacion >= 90){
+    console.log('Tu calificacion es "A"');
+}else if(calificacion >=70){
+    console.log('Tu calificacion es "B"');
+}else if(calificacion >=60){
+    console.log('Tu calificacion es "C"');
+}else if(calificacion >=50){
+    console.log('Tu calificacion es "D"');
+}else{
+    console.log('Tu calificacion es "F"');
+}
+
+//con TERNARIOS
+let puntaje = (calificacion >= 90) ? 'Tu Puntuacion es "A"' :
+              (calificacion >= 70) ? 'Tu Puntuacion es "B"' :
+              (calificacion >= 60) ? 'Tu Puntuacion es "C"' :
+              (calificacion >= 50) ? 'Tu Puntuacion es "D"' :
+              'Tu Puntuacion es "F"';
+
+console.log(puntaje);  
+    
 //R2:Consulta si la temporada es Otoño, Invierno, Primavera o Verano. Si la entrada del usuario es :
 
     //. Septiembre, Octubre o Noviembre, la temporada es Otoño.
     //.. Diciembre, Enero o Febrero, la temporada es Invierno.
     //... Marzo, Abril o Mayo, la temporada es Primavera
     //.... Junio, Julio o Agosto, la temporada es Verano
+//RESUELTO=>
+// con IF & ELSE
+ let temporada = 'junio'//prompt('Ingresa un mes del Año').toLowerCase();
+
+    if( temporada === 'septiembre' || temporada === 'octubre' || temporada === 'noviembre'){
+        console.log('La temporada es Otoño');    
+    } else if ( temporada === 'diciembre' || temporada === 'enero' || temporada === 'febrero'){
+        console.log('La temporada es Invierno');    
+    }else if ( temporada === 'marzo' || temporada === 'abril' || temporada === 'mayo'){
+        console.log('La temporada es Primavera');    
+    }else if( temporada === 'junio' || temporada === 'julio' || temporada === 'agosto'){
+        console.log('La temporada es Verano');    
+    } else{
+        console.log('Ingresaste un dato Incorrecto');
+    };
+//RESUELTO=>
+// con SWITCH
+let mes = temporada
+    switch (mes){
+        case 'enero':
+            console.log("es Invierno");
+            break;
+            case 'febrero':
+            console.log("es Invierno");
+            break;
+            case 'marzo':
+            console.log("es Primavera");
+            break;
+            case 'abril':
+            console.log("es Primavera");
+            break;
+            case 'mayo':
+            console.log("es Primavera");
+            break;
+            case 'junio':
+            console.log("es Verano");
+            break;
+            case 'julio':
+            console.log("es Verano");
+            break;
+            case 'agost':
+            console.log("es Verano");
+            break;
+            case 'septiembre':
+            console.log("es Otoño");
+            break;
+            case 'octubre':
+            console.log("es Otoño");
+            break;
+            case 'noviembre':
+            console.log("es Otoño");
+            break;
+            case 'diciembre':
+            console.log("es Invierno");
+            break;
+            default :
+            console.log('dato incorrecto');
+        }
 
 //R4:Compruebe si un día es un día de fin de semana o un día laborable. Su script tomará el día como entrada.
-
     // ¿Qué día es hoy? Sábado
     // El sábado es fin de semana.
 
@@ -29,3 +110,32 @@ console.log('nivel 2')
 
     // ¿Qué día es hoy? ViErNes
     // El viernes es un día laborable.
+//RESUELTO=>
+// con SWITCH
+let queDiaEsHoy = 'martes'//prompt('introduce un dia').toLowerCase()
+let dia = queDiaEsHoy
+    switch (dia){
+        case 'lunes':
+        console.log(`El ${dia} es un día laborable`);
+        break;
+        case 'martes':
+        console.log(`El ${dia} es un día laborable`);
+        break;
+        case 'miercoles':
+        console.log(`El ${dia} es un día laborable`);
+        break;
+        case 'jueves':
+        console.log(`El ${dia} es un día laborable`);
+        break;
+        case 'viernes':
+        console.log(`El ${dia} es un día laborable`);
+        break;
+        case 'sabado':
+        console.log(`El ${dia} es fin de Semana`);
+        break;
+        case 'domingo':
+        console.log(`El ${dia} es fin de Semana`);
+        break;
+        default :
+        console.log('Debes Introducir un "DIA"');
+        }
